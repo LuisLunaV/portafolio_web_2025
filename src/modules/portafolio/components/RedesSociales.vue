ff73f8dbaf5cffa81f2dc1a0bd91499c1bed8e4e
LuisLunaV <luis.a.luna.v@outlook.com>
2025-05-12 00:21:25 -0600

    Animacion mensaje

--- src/modules/portafolio/components/RedesSociales.vue
+++ src/modules/portafolio/components/RedesSociales.vue
@@ -2,7 +2,7 @@
   <!-- Nota: Los dos !! se usan para convertir un valor cualquiera en su equivalente booleano (true/false).
   Es una forma rapida y comun de asegurar que un valor sea tratado como boolean.
   -->
-      <div v-for="(value, index) in data" :class="activarDivClass(!!value.telefono)" :key="index">
+      <div v-for="(value, index) in data" :class="[nameActive===false?'redes-div-i':'redes-div-sc',{'ocultar-icono':anchoVentana>700 && value.telefono}]" :key="index">
       <a :class="[{'shadow-drop-center':value.name==='Mensaje'}, {'centrar':nameActive}]" :href="value.url" target="_black">
         <i @click="()=>sendMessage(value.name)" :class="[ value.clase,'fa-2x', 'color-blanco', 'sombra-de-texto-3rem']">
         </i>
@@ -13,7 +13,7 @@
 </template>
 
 <script lang="ts" setup>
-import { onMounted, ref } from 'vue';
+import { onBeforeUnmount, onMounted, ref } from 'vue';
 import { redes, type IRedes } from '@/json/redes';
 
 
@@ -27,27 +27,21 @@ interface Props{
   nameActive?:boolean;
 }
 
-const props = withDefaults( defineProps<Props>(),{
+withDefaults( defineProps<Props>(),{
   nameActive:false
 });
 
-const activarDivClass =(telefono:boolean ):(string|object)=>{
-  return [
-    props.nameActive===false?'redes-div-i':'redes-div-sc', //Mostramos o quitamos los nombres de las redes sociales
-    {'ocultar-icono':ocultarIconoTelefono( telefono )} //Ocultamos el icono de telefono si no es un dispisitivo celular
-  ];
-}
-
-const ocultarIconoTelefono=( telefono:boolean):boolean=>{
-  return anchoVentana.value>700 && telefono;
-}
 
 onMounted(()=>{
   window.addEventListener('resize',()=>{
     anchoVentana.value = window.innerWidth;
-    console.log(anchoVentana.value)
   });
 })
+ onBeforeUnmount(() => {
+    window.removeEventListener('resize', ()=>{
+    anchoVentana.value = window.innerWidth;
+    });
+  });
 
 const sendMessage=(message:string)=>{
   console.log(message)
