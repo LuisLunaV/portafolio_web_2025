be54d49eb13128480ce6ebe6775de77708c493d9
LuisLunaV <luis.a.luna.v@outlook.com>
2025-06-23 01:04:07 -0600

    localTunnel api

--- src/modules/services/api.ts
+++ src/modules/services/api.ts
@@ -1,6 +1,6 @@
 // const mensajesApiEnv = import.meta.env.VITE_PORTAFOLIIO_API_URL;
 
-const mensajesApi = 'http://192.168.100.8:3000/panel/api/v1'
+const mensajesApi = 'https://luislunaapiweb.loca.lt/panel/api/v1'
 
 export{
 mensajesApi
